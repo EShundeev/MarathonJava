96583ca5d89d62f2dd5dcecfdb674a74fff39532
EShundeev <gORG.sh@yandex.ru>
2021-07-18 15:50:03 +0500

    complete day7 tasks

--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -1,7 +1,74 @@
 package day7;
+/*
+В классе Самолет реализовать статический метод compareAirplanes,
+который в качестве аргументов принимает два объекта класса Airplane (два самолета)
+и выводит сообщение в консоль о том, какой из самолетов длиннее.
 
+ */
 public class Task1 {
     public static void main(String[] args) {
+    Airplane air11 = new Airplane("Boing", 2010, 30, 10000, 0);
+    Airplane air22 = new Airplane("Sesna", 2020, 250, 5000, 0);
+    Airplane.compareAirplanes(air11,air22);
+    }
 
+    static class Airplane {
+        private String producer;
+        private int year;
+        private int length;
+        private int weight;
+        private int fuel;
+
+        public Airplane(String producer, int year, int length, int weight, int fuel){
+            this.producer = producer;
+            this.year = year;
+            this.length = length;
+            this.weight = weight;
+            this.fuel = 0;
+        }
+        public static void  compareAirplanes(Airplane air1, Airplane air2){
+            if(air1.getLength()>air2.getLength()){
+                System.out.println("Первый самолет длиннее");
+            }
+            else if (air1.getLength()<air2.getLength()){
+                System.out.println("Второй самолет длиннее ");}
+
+            else{
+                System.out.println("Длины самолетов равны");
+            }
+
+        }
+
+        public void setProducer(String producer){
+            this.producer = producer;
+        }
+        public void setYear(int year){
+            this.year = year;
+        }
+        public void setLength(int length){
+            this.length = length;
+        }
+
+        public int getLength() {
+            return length;
+        }
+
+        public void setWeight(int weight){
+            this.weight = weight;
+        }
+        public void setFuel(int fuel){
+            this.fuel = fuel;
+        }
+        public int getFuel(){
+            return fuel;
+        }
+
+        public void info(){
+            System.out.println("Изготовитель: " + producer + ", год выпуска: " + year + ", длина: " + length + ", вес: " + weight + ", количество топлива в баке: " + fuel);
+        }
+
+        public void fillUp(int n){
+            fuel+=n;
+        }
     }
 }
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -1,7 +1,93 @@
 package day7;
 
+import java.util.Random;
+
+/*
+Для игры в футбол во дворе требуется 6 человек (3х3). Класс Игрок (англ. Player) содержит следующие поля:
+Поле “выносливость” (англ. stamina), разное для каждого экземпляра, инициализируется через конструктор.
+Константы “максимальная выносливость” (англ. MAX_STAMINA) со значением 100 и “минимальная выносливость” (англ. MIN_STAMINA) со значением 0, единые для всех экземпляров.
+Статическое поле countPlayers, которое считает количество игроков на футбольном поле (изначально их 0, выходом на поле считается создание экземпляра класса, уходом - когда игрок устал).
+Геттеры для полей “выносливость” и “количество игроков”.
+
+и следующие методы:
+run() - игрок бежит при вызове этого метода. Этот метод уменьшает выносливость игрока на 1 при однократном вызове. Когда выносливость достигает 0, игроку нужен отдых и он уходит с поля.
+info() -  выводит сообщение в зависимости от количества игроков на поле.
+Если игроков меньше 6, то выводит сообщение: “Команды неполные. На поле еще есть ... свободных мест”, иначе: “На поле нет свободных мест”.
+Грамматикой русского языка пренебречь, т.е. фраза “еще есть 1 свободных мест” допустима.
+
+Задание: Создать класс Player по вышеописанному шаблону. В методе main() создать объект класса Random, который будет генерировать случайные числа.
+Создать 6 игроков, передавая им в качестве аргумента в конструктор случайно сгенерированные числа от 90 до 100. Вызвать метод info().
+При попытке создать 7,8 … n игрока, количество игроков на поле меняться не должно, проверить это.
+Примените к одному игроку метод run(), пока он полностью не выдохнется (отрицательное значение выносливости не допускается). Вывести количество игроков на поле.
+
+*По желанию: доработать метод info() так, чтобы при выводе в консоль грамматика русского языка учитывалась.
+
+ */
 public class Task2 {
     public static void main(String[] args) {
+        Random random = new Random();
+        int randStaminaValue = random.nextInt(11 + 1) + 90;
 
+        Player player1 = new Player(randStaminaValue);
+        Player player2 = new Player(randStaminaValue);
+        Player player3 = new Player(randStaminaValue);
+
+        Player.info();
+
+        Player player4 = new Player(randStaminaValue);
+        Player player5 = new Player(randStaminaValue);
+        Player player6 = new Player(randStaminaValue);
+
+        Player.info();
+
+        Player player7 = new Player(randStaminaValue);
+
+        Player.info();
+
+        for(int i = 0; i<101; i++){
+            player3.run();
+        }
+        Player.info();
+    }
+}
+class Player{
+    private int stamina;
+    private final static int  MAX_STAMINA = 100;
+    private final static int MIN_STAMINA = 0;
+    private static int countPlayers;
+
+    public Player(int stamina){
+        this.stamina = stamina;
+        if(countPlayers<6){
+            countPlayers++;
+        }
+    }
+
+    public int getStamina(){
+        return this.stamina;
+    }
+    public int getCountPlayers(){
+        return countPlayers;
+    }
+     public void run(){
+        if(stamina == 0){
+            return;
+        }
+         stamina--;
+
+        if(stamina == 0){
+            countPlayers--;
+        }
+     }
+
+    public static void info(){
+        int point;
+        if(countPlayers<6){
+            point = 6-countPlayers;
+            System.out.println("Команды неполные. На поле еще есть " + point + " свободных мест");
+        }
+        else{
+            System.out.println("На поле нет свободных мест");
+        }
     }
 }

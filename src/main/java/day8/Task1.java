829bb72b1f96b58a70ab8e058d8829ce6ea510a2
EShundeev <gORG.sh@yandex.ru>
2021-08-01 14:35:07 +0500

    complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -1,7 +1,42 @@
 package day8;
+/*
+Создать строку, состоящую из чисел от 0 до 20000.
+Важно понимать, что это одна строка, полученная конкатенацией (“склеиванием”) чисел из диапазона через пробел
+(0 + “ “ + 1 + “ “ + 2 + … + 20000).
+После создания такой строки, вызов System.out.println() на этой строке должен вывести в консоль сразу все числа из диапазона:
+0 1 2 3 4 5 6 7 8 9 10 11 12 … 19995 19996 19997 19998 19999 20000
+
+Для того, чтобы почувствовать разницу в производительности между конкатенацией обычных строк (класс String)
+и использовании StringBuilder, реализуйте описанную задачу этими двумя способами, замеряя время работы программы.
+ */
 
 public class Task1 {
     public static void main(String[] args) {
 
+//        long startTime = System.currentTimeMillis();
+//
+//        String string0_20000 = "";
+//
+//        for(int i = 0; i<=20000; i++){
+//            string0_20000 += i + " ";
+//        }
+//        System.out.println(string0_20000);
+//
+//        long finishTime = System.currentTimeMillis();
+//        long endTime = finishTime - startTime;
+//
+//        System.out.println("Время выполнения: " + endTime);
+
+        long startTime2 = System.currentTimeMillis();
+        StringBuilder sb0_20000 = new StringBuilder("");
+
+        for(int i = 0; i<=20000; i++){
+            sb0_20000.append(i).append(" ");
+        }
+        System.out.println(sb0_20000.toString());
+        long finishTime2 = System.currentTimeMillis();
+        long endTime2 = finishTime2 - startTime2;
+
+        System.out.println("Время выполнения: " + endTime2);
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,54 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
+        Airplane airplane = new Airplane ("Boeing", 2000, 150, 10000);
+        System.out.println(airplane);
+    }
 
+    static class Airplane {
+        private String producer;
+        private int year;
+        private double length;
+        private double weight;
+        private int fuel;
+
+        public Airplane(String producer, int year, double length, double weight){
+            this.producer = producer;
+            this.year = year;
+            this.length = length;
+            this.weight = weight;
+            this.fuel = 0;
+        }
+
+        public void setProducer(String producer){
+            this.producer = producer;
+        }
+        public void setYear(int year){
+            this.year = year;
+        }
+        public void setLength(double length){
+            this.length = length;
+        }
+        public void setWeight(double weight){
+            this.weight = weight;
+        }
+        public void setFuel(int fuel){
+            this.fuel = fuel;
+        }
+        public int getFuel(){
+            return fuel;
+        }
+
+//        public void info(){
+//            System.out.println("Изготовитель: " + producer + ", год выпуска: " + year + ", длина: " + length + ", вес: " + weight + ", количество топлива в баке: " + fuel);
+//        }
+
+        public String toString(){
+            return "Изготовитель: " + producer + ", год выпуска: " + year + ", длина: " + length + ", вес: " + weight + ", количество топлива в баке: " + fuel;
+        }
+
+        public void fillUp(int n){
+            fuel+=n;
+        }
     }
 }
